ba852694b60e96d782407a7df1cf766aa86ec9c4
kensou714 <kensou714@qq.com>
2016-12-11 05:11:33 +0800

    添加金角可选，暂时替换了原版的孙悟空

--- src/game/role/SunWuKong/Input.c
+++ src/game/role/SunWuKong/Input.c
@@ -24,15 +24,17 @@ extern int Input_Ins_22B[];
 
 const InputElem InputTbl_SunWuKong_0[]={
 	//	输入指令		固定	  ActionId	参数		//参数部分详细分析后应在input.h中进行define
-	{Input_Ins_6A,			0x14,	10,	0x4890},		//黑色波动
+
 	{Input_Ins_236A,		0x14,	12,	0x4890},		//
+	{Input_Ins_6A,			0x14,	10,	0x4890},		//黑色波动	
 	{Input_Ins_236A,		0x14,	13,	0x18A0},		//空中
 	{Input_Ins_28A,			0x14,	11,	0x4890},		//
 	{Input_Ins_82A,			0x14,	14,	0x4891},		//
 	{Input_Ins_2A,			0x14,	15,	0x18A3},		//空中
 	//以下曝气后输入
-	{Input_Ins_6A,			0x14,	0,	0x4910},		//黑色波动
+
 	{Input_Ins_236A,		0x14,	2,	0x4910},		//
+	{Input_Ins_6A,			0x14,	0,	0x4910},		//黑色波动	
 	{Input_Ins_236A,		0x14,	3,	0x1920},		//空中
 	{Input_Ins_28A,			0x14,	1,	0x4910},		//
 	{Input_Ins_82A,			0x14,	4,	0x4911},		//
@@ -41,6 +43,7 @@ const InputElem InputTbl_SunWuKong_0[]={
 //	{Input_Ins_2B,			0x14,	28,	0x1810},		//新增加2B突进技测试
 	{Input_Ins_6B,			0x14,	28,	0x1810},		//
 	{Input_Ins_6B,			0x14,	29,	0x1820},		//空中
+	
 	{Input_Ins_236B,		0x14,	21,	0x4890},		//
 	{Input_Ins_46B,		0x14,	20,	0x4850},	//
 	{Input_Ins_28B,		0x14,	22,	0x4852},	//
