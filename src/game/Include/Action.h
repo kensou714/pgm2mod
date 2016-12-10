a0c4e6d5eb390577199ddff36e661cf42c4f9879
kensou714 <kensou714@qq.com>
2016-12-10 17:31:18 +0800

    增加人物脚本

---
+++ src/game/Include/Action.h
@@ -0,0 +1,71 @@
+
+//ACT类型定义
+#define ACT_WAIT			0
+#define ACT_START			1
+#define ACT_MOTION			2
+#define ACT_ATTACK			3
+#define ACT_KILL			6
+#define ACT_CANING			8
+#define ACT_VICTOR			9
+
+//ACT数量
+#define ACT_WAIT_NUM	    45
+#define ACT_START_NUM	    0xE
+#define ACT_MOTION_NUM		0x3A
+#define ACT_ATTACK_NUM		13
+#define ACT_KILL_NUM		0x55
+#define ACT_CANING_NUM		0x1B
+#define ACT_VICTOR_NUM		4
+
+#define ACT_TYPE_NUM		12
+
+//系统特效
+#define SO_DUST				0
+#define SO_BON				1
+#define SO_BLOOD			2
+#define SO_EFFECT			3
+#define SO_QUAKET			4
+
+//动作调用结构
+typedef struct
+{
+	char ActType;				//动作类型
+	char ActId;				//动作ID(当前类型)
+	short Reversed;				//保留 0 
+	int ActScr;				//动作脚本指针
+	short Param1;				//参数1
+	short Param2;				//参数2
+
+}ActElem;
+
+//动作类型指针表结构
+typedef struct ActTypePtrTbl
+{
+	const ActElem* ActWait;
+	const ActElem*  ActStart;
+	const ActElem*  ActMotion;
+	const ActElem*  ActAttack;
+
+	const ActElem* ActType4;
+	const ActElem* ActType5;
+
+	const ActElem* ActKill;
+	const ActElem* ActType7;
+	const ActElem* ActCaning;
+	const ActElem* ActVictor;
+
+	const ActElem* ActType10;		//0
+	const ActElem* ActType11;		//0	
+
+}ActTypePtrTbl;
+
+
+//动作脚本处理
+
+//脚本结束标志
+#define SCR_END 0x8000
+
+
+
+
+
